f372dcf831602cb7d81f15ee911067a684f1546a
Tiarli Oliveira <tiarlioliveira@gmail.com>
2019-06-14 16:15:51 -0300

    Update coverage to 100%

--- spec/awesome_distance_calculator_spec.rb
+++ spec/awesome_distance_calculator_spec.rb
@@ -8,11 +8,27 @@ RSpec.describe AwesomeDistanceCalculator do
   end
 
   context '.calculate' do
-    it 'calculates distance using default haversine strategy' do
-      expect(instance.calculate(coordinates: [
-                                  [53.339428, -6.257664],
-                                  [52.986375, -6.043701]
-                                ])).to eql 41.81553584459387
+    context 'when not passing any strategy' do
+      it 'calculates distance using default haversine strategy' do
+        expect(instance.calculate(coordinates: [
+                                    [53.339428, -6.257664],
+                                    [52.986375, -6.043701]
+                                  ])).to eql 41.81553584459387
+      end
+    end
+
+    context 'when passing a non defined strategy' do
+      it 'raises StrategyNotYetImplemented' do
+        expect do
+          instance.calculate(coordinates: [
+                               [53.339428, -6.257664],
+                               [52.986375, -6.043701]
+                             ],
+                             strategy: 'foo_bar')
+        end.to raise_exception(
+          AwesomeDistanceCalculator::StrategyNotYetImplemented
+        )
+      end
     end
   end
 end
